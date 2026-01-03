d54beb6070293c23c318a761b170b8096f974f5c
Jin <jinny2541@gmail.com>
2026-01-03 23:17:35 +0700

    edited function Post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,50 @@
-function Posts() {
+import { useState } from "react";
+import { postData } from "../raw-data/post-data";
+
+function PostItem({ post }) {
+  const [counterLike, setCounterLike] = useState(post.likes);
+  const handleLikeClick = () => {
+    setCounterLike(counterLike + 1);
+  };
+  const handleDislikeClick = () => {
+    if (counterLike > 0) {
+      setCounterLike(counterLike - 1);
+    }
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
+    <li className="post-item">
+      <div className="post-header">
+        <h2>{post.title}</h2>
+        <div className="post-social-media-stats">
+          <span className="stats-topic">Likes: </span>
+          <span className="post-likes">{counterLike}</span>
         </div>
       </div>
+
+      <p className="post-content">{post.content}</p>
+
+      <div className="post-actions">
+        <button className="like-button" onClick={handleLikeClick}>
+          Like
+        </button>
+        <button className="dislike-button" onClick={handleDislikeClick}>
+          Dislike
+        </button>
+      </div>
+    </li>
+  );
+}
+
+function Posts() {
+  return (
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <ul className="post-list">
+        {postData.map((post) => (
+          <PostItem key={post.id} post={post} />
+        ))}
+      </ul>
     </div>
   );
 }
